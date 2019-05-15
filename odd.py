c0e6de8098ab06c19b0f517d106a7c41c497e1da
manujillu <50619892+manujillu@users.noreply.github.com>
2019-05-15 09:01:27 +0530

    Update odd.py

---
+++ odd.py
@@ -0,0 +1,9 @@
+
+value=int(input())
+if(value<0):
+    print("invalid")
+else:
+  if(value%2==0):
+    print("Even")
+  else:
+    print("Odd")
--- positive.py
+++
@@ -1,7 +0,0 @@
-value=int(input())
-if(value>0):
-  print("Positive")
-elif(value<0):
-  print("Negative")
-else:
-  print("Zero")
